bfb527c01d786a72c58bcaa05974cd1a44e511e8
Elvis <elvisboateng557@gmail.com>
2025-10-20 11:41:35 -1200

    Update WorkoutContext.js

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,17 +1,16 @@
 import { createContext, useReducer } from "react";
 
 export const WorkoutContext = createContext();
-
 export const WorkoutsContext = WorkoutContext;
 
 export const workoutsReducer = (state, action) => {
   switch (action.type) {
     case "SET_WORKOUTS":
-      return { ...state, workouts: action.payload };
+      return { workouts: action.payload };
     case "CREATE_WORKOUT":
-      return { ...state, workouts: [action.payload, ...(state.workouts || [])] };
+      return { workouts: [action.payload, ...(state.workouts || [])] };
     case "DELETE_WORKOUT":
-      return { ...state, workouts: state.workouts.filter(w => w._id !== action.payload) };
+      return { workouts: state.workouts.filter(w => w._id !== action.payload._id) };
     default:
       return state;
   }
@@ -19,10 +18,9 @@ export const workoutsReducer = (state, action) => {
 
 export const WorkoutsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(workoutsReducer, { workouts: [] });
-
  
   return (
-    <WorkoutContext.Provider value={[state.workouts, dispatch]}>
+    <WorkoutContext.Provider value={{ ...state, dispatch }}>
       {children}
     </WorkoutContext.Provider>
   );
